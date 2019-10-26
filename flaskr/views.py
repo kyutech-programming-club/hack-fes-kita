c58b684a7597ea72a07389fdc6c10798ec51ea1a
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-10-26 19:03:33 +0900

    Create Event class to register event

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,67 +1,74 @@
 from flask import request, redirect, url_for, render_template, flash, abort, jsonify
 from flaskr import app, db
-from flaskr.models import Entry, User
+from flaskr.read_csv import get_empty_room_num
+from flaskr.models import Event, Entry
 
-@app.route('/')
-def show_entries():
-    entries = Entry.query.order_by(Entry.id.desc()).all()
-    return render_template('show_entries.html', entries=entries)
+#@app.route('/')
+#def show_entries():
+#    entries = Entry.query.order_by(Entry.id.desc()).all()
+#    return render_template('show_entries.html', entries=entries)
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    entry = Entry(
-            title=request.form['title'],
-            text=request.form['text']
-            )
-    db.session.add(entry)
-    db.session.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+#@app.route('/add', methods=['POST'])
+#def add_entry():
+#    entry = Entry(
+#            title=request.form['title'],
+#            text=request.form['text']
+#            )
+#    db.session.add(entry)
+#    db.session.commit()
+#    flash('New entry was successfully posted')
+#    return redirect(url_for('show_entries'))
 
 
-@app.route('/users/create/', methods=['GET', 'POST'])
-def user_create():
+
+
+@app.route('/events/create/', methods=['GET', 'POST'])
+def event_create():
     if request.method == 'POST':
-        user = User(name=request.form['name'],
-                    email=request.form['email'],
-                    password=request.form['password'])
-        db.session.add(user)
+        event = Event(title=request.form['title'],
+                    description=request.form['description'],
+                    day=request.form['day'],
+                    room=request.form['room'],
+                    time=request.form['time'])
+        db.session.add(event)
         db.session.commit()
-        return redirect(url_for('user_list'))
-    return render_template('user/edit.html')
+        return redirect(url_for('event_list'))
+    return render_template('event/edit.html')
 
-@app.route('/users/')
-def user_list():
-    users = User.query.all()
-    return render_template('user/list.html', users=users)
+@app.route('/events/')
+def event_list():
+    events = Event.query.all()
+    return render_template('event/list.html', events=events)
 
-@app.route('/users/<int:user_id>/')
-def user_detail(user_id):
-    user = User.query.get(user_id)
-    return render_template('user/detail.html', user=user)
+@app.route('/events/<int:event_id>/')
+def event_detail(event_id):
+    event = Event.query.get(event_id)
+    return render_template('event/detail.html', event=event)
 
-@app.route('/users/<int:user_id>/edit/', methods=['GET', 'POST'])
-def user_edit(user_id):
-    user = User.query.get(user_id)
-    if user is None:
+@app.route('/events/<int:event_id>/edit/', methods=['GET', 'POST'])
+def event_edit(event_id):
+    event = Event.query.get(event_id)
+    if event is None:
         abort(404)
     if request.method == 'POST':
-        user.name=request.form['name']
-        user.email=request.form['email']
-        user.password=request.form['password']
-        db.session.add(user)
+        event.title=request.form['title']
+        event.description=request.form['description']
+        event.day=request.form['day']
+        event.room=request.form['room']
+        event.time=request.form['time']
+        db.session.add(event)
         db.session.commit()
-        return redirect(url_for('user_detail', user_id=user_id))
-    return render_template('user/edit.html', user=user)
+        return redirect(url_for('event_detail', event_id=event_id))
+    return render_template('event/edit.html', event=event)
 
-@app.route('/users/<int:user_id>/delete/', methods=['DELETE'])
-def user_delete(user_id):
-    user = User.query.get(user_id)
-    if user is None:
+@app.route('/events/<int:event_id>/delete/', methods=['DELETE'])
+def event_delete(event_id):
+    event = Event.query.get(event_id)
+    if event is None:
         response = jsonify({'status': 'Not Found'})
         response.status_code = 404
         return response
-    db.session.delete(user)
+    db.session.delete(event)
     db.session.commit()
     return jsonify({'status': 'OK'})
 
